29f8611958d27eb0979ba4df8b6baac661949c9d
Totus tuo <totustuo@MacBook-Pro-de-Totus.local>
2024-04-18 08:57:09 -0600

    First commit, but secction namespace is finished already

---
+++ src/data/powers.ts
@@ -0,0 +1,19 @@
+
+export interface Power {
+    id: number;
+    description: string;
+}
+
+// export const powers: Power[] = [
+const powers: Power[] = [
+    {
+        id: 1,
+        description: 'Money',
+    },
+    {
+        id: 2,
+        description: 'Drugs',
+    }
+];
+
+export default powers;
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import { powers } from "./data/powers";
+// import powers from "./data/powers";
+
+// const Hero = 123;
+
+// const ironman = new SuperHero('Ironman', 1, 55);
+const ironman = new Hero('Ironman', 1, 55);
+
+console.log(ironman);
+
+console.log(ironman.power);
